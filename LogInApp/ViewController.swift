654df56f52654a8deee18977149482ac32c6a408
Dima Monid <1234567waka.waka@gmail.com>
2025-01-22 20:21:29 +0300

    Initial Commit

---
+++ LogInApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInApp
+//
+//  Created by Дима Монид on 22.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
